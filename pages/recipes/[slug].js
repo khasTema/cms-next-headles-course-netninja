052bf3af2ab207d57014cbfe460025ff0e5b3d51
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-05-24 13:43:45 +0200

    lesson 7 | fetch one recipe

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,51 @@
-export default function RecipeDetails() {
+import { createClient } from "contentful"
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+
+// get all items fro multimple static page generation
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({content_type: 'recepie'})
+
+  const paths = res.items.map(item => {
+    return {
+      params: {
+        slug: item.fields.slug
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+// get one item fro this particular page
+// context has params fro this page slug-link
+export const getStaticProps = async ({params}) => {
+  const { items } = await client.getEntries({
+    content_type: 'recepie',
+    'fields.slug': params.slug
+  })
+
+  return {
+    props: {
+      recipe: items[0]
+    }
+  }
+}
+
+
+export default function RecipeDetails({recipe}) {
   return (
     <div>
       Recipe Details
+      <br />
+      {recipe.fields.title}
     </div>
   )
 }
