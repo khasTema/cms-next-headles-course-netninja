f941efa7be80999431f2c870aef0b56400a87f95
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-05-25 23:06:11 +0200

    upt

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
     <div className="layout">
       <Head>
         <title>Marmite All</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <header>
         <Link legacyBehavior href="/">
