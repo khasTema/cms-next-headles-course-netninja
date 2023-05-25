b3ae64a58b4f6c389e0d018e2d530c75f72ddcdb
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-05-25 23:04:02 +0200

    adding head

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,14 @@
 import Link from 'next/link'
+import Head from 'next/head'
+
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
+      <Head>
+        <title>Marmite All</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
+      </Head>
       <header>
         <Link legacyBehavior href="/">
           <a>
