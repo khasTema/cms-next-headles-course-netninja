6a41210d4c9f0ac3c786d13c67fa76437184affb
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-05-26 09:16:36 +0200

    meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -6,8 +6,10 @@ export default function Layout({ children }) {
   return (
     <div className="layout">
       <Head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Marmite All</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <header>
         <Link legacyBehavior href="/">
